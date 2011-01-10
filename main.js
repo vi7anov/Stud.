c927b8fddfed7ac4a424120a51f4eef75d676066
unknown <Sky_is_The_Limit@mail.bg>
2011-01-10 11:25:55 +0200

    filter

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 (function(document,$,undefined){
   $(function(){
    
+    $('#filter').bind("keyup",function() {
+    
+     $('ul li').each(function(){ 
+      if($(this).text().toLowerCase().match($('#filter').val().toLowerCase()))       
+      $(this).show();
+    else 
+       $(this).hide();
     
     
-    
-    
-    
-    
-    
-    
-    
-
-    
+       });
+    });
   });
-  
-  
-  
   
 })(document,jQuery);
